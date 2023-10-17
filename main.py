23ed74d529bb6400aa2f98ecd320c71d03ed64b7
Mindaugas Minelga <minelga.m@gmail.com>
2023-10-17 18:07:13 +0300

    add main.py file

---
+++ main.py
@@ -0,0 +1,48 @@
+# all_users = {"aaa": "111", "bbb": "222", "ccc": "333"}
+# value = True
+# while value == True:
+#     entered_username = input("Enter your username: ")
+#     entered_password = input("Enter your password: ")
+    
+#     for username, password in all_users.items():
+#         if entered_username == username and entered_password == password:
+#             print("Welcome")
+#             value = False
+#             break
+#     else:
+#         print("Wrong")
+
+# all_users = {"aaa": "111", "bbb": "222", "ccc": "333"}
+# while True:
+#     entered_username = input("Enter your username: ")
+#     entered_password = input("Enter your password: ")
+#     # if username in all_users and all_users[username] == password: # si eilute is GPTchat
+#     for username, password in all_users.items():
+#         user_exists = entered_username == username and entered_password == password
+#         if user_exists:
+#             break
+#     if user_exists:
+#         break
+
+# print("welcome")
+
+# names = ["Albert", "Alma", "Joseph", "Zoro"]
+# print([name for name in names if name.startswith("A") or "e" in name])
+
+# squares = {i: i * i for i in range(10)}
+# print(squares)
+
+# for f_1 in chars:
+#     for f_2 in chars:
+#         for f_3 in chars:
+#             for f_4 in chars:
+#                 if f_1+f_2+f_3+f_4 == pin:
+#                     print(f_1+f_2+f_3+f_4)
+#                     break
+
+# squares = [number for number in range(1, 1001) if number % 6 == 0]
+# print(squares)
+
+# squares = [number for number in range(1, 1001) if "9" in str(number)]
+# print(squares)
+
